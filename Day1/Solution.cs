b550e7a4c286ec968e4059f3007bcd192eaf8f10
Andrii Gastello <concof.zp@gmail.com>
2024-12-01 00:55:52 -0500

    Part 2 Solved

--- Day1/Solution.cs
+++ Day1/Solution.cs
@@ -4,7 +4,7 @@ namespace Day1;
 
 public class Solution
 {
-    public static int Solve(string filename)
+    public static int Solve1(string filename)
     {
         string[] input = File.ReadAllLines(filename);
         int sum = 0;
@@ -38,4 +38,33 @@ public class Solution
 
         return sum;
     }
+    
+    public static int Solve2(string filename)
+    {
+        string[] input = File.ReadAllLines(filename);
+        int sum = 0;
+
+        List<int> leftColumn = new List<int>();
+        List<int> rightColumn = new List<int>();
+
+        foreach (var line in input)
+        {
+            var split = line.Split("   ");
+        
+            leftColumn.Add(int.Parse(split[0])); // Add the first column to the leftColumn list
+            rightColumn.Add(int.Parse(split[1])); // Split the second column by spaces
+        }
+    
+        leftColumn.Sort();
+        rightColumn.Sort();
+
+        foreach (var t in leftColumn)
+        {
+            var count = rightColumn.Count(t1 => t == t1);
+
+            sum += count * t;
+        }
+        return sum;
+    }
+    
 }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ class Program
     static void Main()
     {
         var Solution = new Solution();
-        Console.WriteLine(Solution.Solve(@"Z:\source\AdventOfCode2024\Day1\input.txt"));
+        
+        Console.WriteLine(Solution.Solve1(@"Z:\source\AdventOfCode2024\Day1\input.txt"));
+        Console.WriteLine(Solution.Solve2(@"Z:\source\AdventOfCode2024\Day1\input.txt"));
     }
 }
