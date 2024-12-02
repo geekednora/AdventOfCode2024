e470675dec5ae061387973834adc5d9355634132
Andrii Gastello <concof.zp@gmail.com>
2024-12-02 15:54:05 -0500

    Part 1 solved

---
+++ Day2/Solution.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace AdventOfCode2024.Day2;
+
+public class Solution
+{
+    public int Solve1(string filename)
+    {
+        string[] input = GetInput(filename);
+        int sum = 0;
+
+        foreach (var line in input)
+        {
+            var split = line.Split(" ");
+            
+            bool isIncreasing = true;
+            bool isDecreasing = true;
+            
+            for (int i = 0; i < split.Length-1; i++)
+            {
+                int diff = Math.Abs(int.Parse(split[i + 1]) - int.Parse(split[i]));
+                if (diff is < 1 or > 3)
+                {
+                    isIncreasing = false;
+                    isDecreasing = false;
+                    break;
+                }
+                if (int.Parse(split[i]) < int.Parse(split[i+1]))
+                {
+                    isDecreasing = false;
+                }
+                if (int.Parse(split[i]) > int.Parse(split[i+1]))
+                {
+                    isIncreasing = false;
+                }
+            }
+
+            if (isIncreasing || isDecreasing)
+            {
+                sum++;
+            }
+        }
+
+        return sum;
+    }
+    
+    public int Solve2(string filename)
+    {
+        string[] input = GetInput(filename);
+        int sum = 0;
+
+        foreach (var line in input)
+        {
+            var split = line.Split(" ");
+            
+            bool isIncreasing = true;
+            bool isDecreasing = true;
+            
+            for (int i = 0; i < split.Length-1; i++)
+            {
+                int diff = Math.Abs(int.Parse(split[i + 1]) - int.Parse(split[i]));
+                if (diff is < 1 or > 3)
+                {
+                    isIncreasing = false;
+                    isDecreasing = false;
+                    break;
+                }
+                if (int.Parse(split[i]) < int.Parse(split[i+1]))
+                {
+                    isDecreasing = false;
+                }
+                if (int.Parse(split[i]) > int.Parse(split[i+1]))
+                {
+                    isIncreasing = false;
+                }
+            }
+
+            if (isIncreasing || isDecreasing)
+            {
+                sum++;
+            }
+        }
+
+        return sum;
+    }
+
+
+    /// <summary>
+    /// Receiving the input from the file
+    /// </summary>
+    /// <param name="filename">Absolute path to the file</param>
+    /// <returns>string[] array containing multiple lines from the input file</returns>
+    private static string[] GetInput(string filename) => File.ReadAllLines(filename);
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using Day1;
+﻿namespace AdventOfCode2024;
 
-class Program
+internal static class Program
 {
     static void Main()
     {
-        var Solution = new Solution();
+        var solution = new AdventOfCode2024.Day2.Solution();
         
-        Console.WriteLine(Solution.Solve1(@"Z:\source\AdventOfCode2024\Day1\input.txt"));
-        Console.WriteLine(Solution.Solve2(@"Z:\source\AdventOfCode2024\Day1\input.txt"));
+        Console.WriteLine(solution.Solve1(@"Z:\source\AdventOfCode2024\Day2\input.txt"));
+        Console.WriteLine(solution.Solve2(@"Z:\source\AdventOfCode2024\Day2\input.txt"));
     }
 }
