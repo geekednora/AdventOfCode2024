2a5805ba3d135db16439ef7a147c194fa35471b7
Andrii Gastello <concof.zp@gmail.com>
2024-12-03 23:10:55 -0500

    Part 1 solved

---
+++ Day3/Solution.cs
@@ -0,0 +1,77 @@
+﻿namespace AdventOfCode2024.Day3;
+
+public class Solution
+{
+    public int Solve1(string filename)
+    {
+        var input = GetInput(filename);
+        var sum = 0;
+
+        foreach (var line in input)
+        {
+            if (line.Contains("mul("))
+            {
+                var split = line.Split("mul(")[1..];
+                foreach (var t in split)
+                {
+                    int i = 0;
+                    string multipliers = new string("");
+
+                    // checking for digits before comma in mul
+                    while (char.IsDigit(t[i]))
+                    {
+                        multipliers += t[i];
+                        i++;
+                    }
+
+                    if (!char.IsDigit(t[i]) && t[i] != ',')
+                    {
+                        continue;
+                    }
+
+                    var operand = int.Parse(multipliers);
+                    multipliers = "";
+                    i++;
+
+                    // checking for the second digit if the comma is contained in operation
+                    while (char.IsDigit(t[i]))
+                    {
+                        multipliers += t[i];
+                        i++;
+                    }
+
+                    if (!char.IsDigit(t[i]) && t[i] != ')')
+                    {
+                        continue;
+                    }
+
+                    operand *= int.Parse(multipliers);
+                    sum += operand;
+                }
+            }
+
+        }
+
+        return sum;
+    }
+
+    public int Solve2(string filename)
+    {
+        var input = GetInput(filename);
+        var sum = 0;
+        
+        
+        return sum;
+    }
+
+
+    /// <summary>
+    ///     Receiving the input from the file
+    /// </summary>
+    /// <param name="filename">Absolute path to the file</param>
+    /// <returns>string[] array containing multiple lines from the input file</returns>
+    private static string[] GetInput(string filename)
+    {
+        return File.ReadAllLines(filename);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ internal static class Program
 {
     static void Main()
     {
-        var solution = new AdventOfCode2024.Day2.Solution();
+        var solution = new AdventOfCode2024.Day3.Solution();
         
-        Console.WriteLine(solution.Solve1(@"Z:\source\AdventOfCode2024\Day2\input.txt"));
-        Console.WriteLine(solution.Solve2(@"Z:\source\AdventOfCode2024\Day2\input.txt"));
+        Console.WriteLine(solution.Solve1(@"Z:\source\AdventOfCode2024\Day3\input.txt"));
+        //Console.WriteLine(solution.Solve2(@"Z:\source\AdventOfCode2024\Day3\input.txt"));
     }
 }
