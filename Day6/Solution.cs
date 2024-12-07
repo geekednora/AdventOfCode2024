43eb5bd11ef4b2947f0abf545aae3075d2e1789c
Andrii Gastello <concof.zp@gmail.com>
2024-12-06 22:38:53 -0500

    Part 2 solved

--- Day6/Solution.cs
+++ Day6/Solution.cs
@@ -6,36 +6,87 @@ public class Solution
     {
         /* Default variables */
         var input = GetInput(filename);
-        var sum = 0;
         /* End of default */
-        
-        char[][] map = input.Select(line => line.ToCharArray()).ToArray();
-        
+
+        var map = input.Select(line => line.ToCharArray()).ToArray();
+
         const char guard = '^';
         const char obstacle = '#';
         const char visited = 'X';
         int guardPosX = 0, guardPosY = 0;
-        int direction = 0; // 0 = up, 1 = right, 2 = down, 3 = left
+        var direction = 0; // 0 = up, 1 = right, 2 = down, 3 = left
 
-        bool isOut = false;
         HashSet<(int, int)> visitedPos = [];
 
-        for (int y = 0; y < map.Length; y++)
-        {
-            for (int x = 0; x < map.First().Length; x++)
+        for (var y = 0; y < map.Length; y++)
+        for (var x = 0; x < map.First().Length; x++)
+            if (map[y][x] == guard)
             {
-                if (map[y][x] == guard)
-                {
-                    guardPosX = x;
-                    guardPosY = y;
-                }
+                guardPosX = x;
+                guardPosY = y;
             }
-        }
-        
+
         visitedPos.Add((guardPosY, guardPosX));
         map[guardPosY][guardPosX] = visited;
 
-        while (!isOut)
+        return CheckPath(guardPosX, guardPosY, direction, map, obstacle, visited, visitedPos);
+    }
+
+    
+
+    public int Solve2(string filename)
+    {
+        /* Default variables */
+        var input = GetInput(filename);
+        int result = 0;
+        var field = new char[input[0].Length, input.Length];
+        var currentX = 0;
+        var currentY = 0;
+        var direction = 0;
+        var lengthX = input[0].Length;
+        var lengthY = input.Length;
+
+        for (var y = 0; y < lengthY; y++)
+        {
+            var line = input[y];
+            for (int x = 0; x < lengthX; x++)
+            {
+                var ch = line[x];
+                if (ch == '^') 
+                {
+                    currentX = x;
+                    currentY = y;
+                    field[x, y] = '.';
+                    continue;
+                }
+                field[x, y] = ch;
+            }
+        }
+
+        for (int x = 0; x < lengthX; x++)
+        {
+            for (int y = 0; y < lengthY; y++)
+            {
+                if (field[x, y] != '#' && !(x == currentX && y == currentY))
+                {
+                    field[x, y] = '#';
+                    if (!CanExit(field, currentX, currentY, direction))
+                    {
+                        result++;
+                        // Console.WriteLine($"x: {x}, y: {y}");
+                    }
+                    field[x, y] = '.';
+                }
+            }
+        }
+        return result;
+    }
+    
+    
+    private int CheckPath(int guardPosX, int guardPosY, int direction, char[][] map, char obstacle, char visited,
+        HashSet<(int, int)> visitedPos)
+    {
+        while (true)
         {
             int nextPosX = guardPosX, nextPosY = guardPosY;
 
@@ -55,11 +106,10 @@ public class Solution
                     break; // left
             }
 
-            
-            if (nextPosX < 0 || nextPosX >= map[0].Length 
+
+            if (nextPosX < 0 || nextPosX >= map[0].Length
                              || nextPosY < 0 || nextPosY >= map.Length)
             {
-                isOut = true;
                 break;
             }
             if (map[nextPosY][nextPosX] == obstacle)
@@ -70,32 +120,81 @@ public class Solution
             {
                 guardPosX = nextPosX;
                 guardPosY = nextPosY;
-                
-                
-                if (map[guardPosY][guardPosX] != visited)
+
+
+                if (visitedPos.Contains((guardPosY, guardPosX)))
                 {
-                    visitedPos.Add((guardPosY, guardPosX));
-                    map[guardPosY][guardPosX] = visited;
+                    return -1; // guard is stuck in a loop
                 }
+                visitedPos.Add((guardPosY, guardPosX));
+                map[guardPosY][guardPosX] = visited;
             }
         }
 
         return visitedPos.Count;
     }
-
-    public int Solve2(string filename)
+    
+    private static bool CanExit(char[,] field, int x, int y, int direction)
     {
-        var input = GetInput(filename);
-        var sum = 0;
+        var visitedTiles = new HashSet<(int x, int y, int direction)>();
+        while (true)
+        {
+            if (visitedTiles.Contains((x, y, direction)))
+            {
+                return false;
+            }
+            visitedTiles.Add((x, y, direction));
+            
+            var moveX = 0;
+            var moveY = 0;
+            if (direction == 0)
+            {
+                moveX = 0;
+                moveY = -1;
+            }
+            else if (direction == 1)
+            {
+                moveX = 1;
+                moveY = 0;
+            }
+            else if (direction == 2)
+            {
+                moveX = 0;
+                moveY = 1;
+            }
+            else if (direction == 3)
+            {
+                moveX = -1;
+                moveY = 0;
+            }
 
-        return sum;
+            var nextX = x + moveX;
+            var nextY = y + moveY;
+            if (IsOutOfBounds(nextX, nextY, field.GetLength(0), field.GetLength(1)))
+            {
+                return true;
+            }
+            
+
+            if (field[nextX, nextY] == '#')
+            {
+                direction = (direction + 1) % 4;
+                continue;
+            }
+            
+            x = nextX;
+            y = nextY;
+        }
     }
+    
+    private static bool IsOutOfBounds(int x, int y, int lengthX, int lengthY)
+    {
+        return (x < 0 || y < 0 || x >= lengthX || y >= lengthY);
+    }
+    
 
     private static string[] GetInput(string filename)
     {
         return File.ReadAllLines(filename);
     }
-    
-    
-    //private char[,] GetObstacles(var input)
 }
