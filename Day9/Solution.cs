e6a0fa3643eb6c0bda019a6e5b5c039982714843
Andrii Gastello <concof.zp@gmail.com>
2024-12-11 01:46:03 -0500

    Part 2 save

--- Day9/Solution.cs
+++ Day9/Solution.cs
@@ -1,43 +1,38 @@
-﻿using System.ComponentModel.Design;
-using System.Runtime.InteropServices.ComTypes;
-
-namespace AdventOfCode2024.Day9;
+﻿namespace AdventOfCode2024.Day9;
 
 public class Solution
 {
     public long Solve1(string filename)
     {
         /* Default variables */
-        string input = GetInput(filename);
+        var input = GetInput(filename);
         long sum = 0;
         /* End of default */
 
         var fragments = FileFragmentation(input);
 
-        List<int> blocks = fragments.blocks.ToList();
-        List<int> spaces = fragments.spaces.ToList(); // division of the string into two types of inputs
+        var blocks = fragments.blocks.ToList();
+        var spaces = fragments.spaces.ToList(); // division of the string into two types of inputs
 
-        List<string> diskmap = new List<string>();
+        var diskmap = new List<string>();
         GetDiskmap(blocks, spaces, diskmap);
 
         diskmap = diskmap.ToIndividual();
-        var result = Data.Sort(diskmap);
-        
-        Console.WriteLine(string.Join("", result));
+        Data.Sort(diskmap);
 
-        for (int i = 0; i < diskmap.Count; i++)
+        var result = Data.Sort(diskmap);
+
+        Console.WriteLine("DEBUG:   " + string.Join("", result), Console.ForegroundColor = ConsoleColor.Red);
+
+
+        for (var i = 0; i < diskmap.Count; i++)
         {
-            if (diskmap[i] == ".")
-            {
-                break;
-            }
+            if (diskmap[i] == ".") break;
             sum += int.Parse(diskmap[i]) * i;
         }
-        
+
         return sum;
     }
-
-    
 
 
     public long Solve2(string filename)
@@ -47,54 +42,64 @@ public class Solution
         long sum = 0;
         /* End of default */
 
+        var fragments = FileFragmentation(input);
+
+        var blocks = fragments.blocks.ToList();
+        var spaces = fragments.spaces.ToList(); // division of the string into two types of inputs
+
+        var diskmap = new List<string>();
+        GetDiskmap(blocks, spaces, diskmap);
+
+        diskmap = diskmap.ToIndividual();
+        Data.Sort(diskmap);
+
+        var result = diskmap.Fit();
+
+        Console.WriteLine("DEBUG:   " + string.Join("", result), Console.ForegroundColor = ConsoleColor.Red);
+
+
+        for (var i = 0; i < diskmap.Count; i++)
+        {
+            if (diskmap[i] == ".") break;
+            sum += int.Parse(diskmap[i]) * i;
+        }
+
         return sum;
     }
 
-    
-    
-    
+
     private static void GetDiskmap(List<int> blocks, List<int> spaces, List<string> diskmap)
     {
-        bool type = true;
-        for (int i = 0; i < blocks.Count + spaces.Count;)
-        {
+        var type = true;
+        for (var i = 0; i < blocks.Count + spaces.Count;)
             switch (type)
             {
                 case true:
                 {
-                    if (blocks.Count > i)
-                    {
-                        diskmap.Add( blocks[i].ToString() );
-                    }
+                    if (blocks.Count > i) diskmap.Add(blocks[i].ToString());
 
                     type = false;
                     break;
                 }
                 case false:
                 {
-                    if (spaces.Count > i)
-                    {
-                        diskmap.Add( spaces[i].ToString() );
-                    }
-                    
+                    if (spaces.Count > i) diskmap.Add(spaces[i].ToString());
+
                     i++;
                     type = true;
                     break;
                 }
             }
-        }
     }
-    
+
     private static (List<int> blocks, List<int> spaces) FileFragmentation(string input)
     {
-        List<int> blocks = new List<int>();
-        List<int> spaces = new List<int>();
-        
-        bool type = true;
-        
+        var blocks = new List<int>();
+        var spaces = new List<int>();
+
+        var type = true;
+
         foreach (var t in input)
-        {
-            
             switch (type)
             {
                 case true:
@@ -110,78 +115,106 @@ public class Solution
                     break;
                 }
             }
-        }
+
         return (blocks, spaces);
     }
-    
+
     // Receiving input from the file
     private static string GetInput(string filename)
     {
         return File.ReadAllLines(filename)[0];
     }
-    
 }
 
 public static class Data
 {
     private const char space = '.';
-    private static List<string> set = new List<string>();
-    
+    private static List<string> set = new();
+
     public static List<string> ToIndividual(this List<string> diskmap)
     {
-        bool type = true;
-        int id = 0;
+        var type = true;
+        var id = 0;
+
+        set = new List<string>(); // resetting data in set
 
         foreach (var t in diskmap)
-        {
             switch (type)
             {
                 case true:
                 {
-                    for (int i = 0; i < int.Parse(t); i++)
-                    {
-                        set.Add(id.ToString());
-                    }
+                    for (var i = 0; i < int.Parse(t); i++) set.Add(id.ToString());
 
                     id++;
-                    
+
                     type = false;
                     break;
                 }
                 case false:
                 {
-                    for (int i = 0; i < int.Parse(t); i++)
-                    {
-                        set.Add(space.ToString());
-                    }
-                    
+                    for (var i = 0; i < int.Parse(t); i++) set.Add(space.ToString());
+
                     type = true;
                     break;
                 }
             }
-        }
 
         return set;
     }
 
     public static List<string> Sort(this List<string> data)
     {
-        List<string> result = new List<string>();
-        for (int i = 0; i < data.Count; i++)
-        {
+        var result = new List<string>();
+        for (var i = 0; i < data.Count; i++)
             if (data[i] == space.ToString())
-            {
-                for (int j = data.Count - 1; j >= i; j--)
-                {
+                for (var j = data.Count - 1; j >= i; j--)
                     if (data[j] != space.ToString())
                     {
                         data[i] = data[j];
                         data[j] = space.ToString();
                         break;
                     }
+
+        return data;
+    }
+
+    public static List<string> Fit(this List<string> data)
+    {
+        for (var i = 0; i < data.Count; i++)
+            if (data[i] == space.ToString())
+            {
+                var count = 1;
+                for (var j = i + 1; j < data.Count; j++) // counting dots
+                {
+                    if (data[j] != space.ToString()) break;
+
+                    count++;
+                }
+
+                for (var j = data.Count - 1; j >= i; j--)
+                {
+                    var nums = 0;
+                    if (int.TryParse(data[j], out var temp))
+                        for (var c = j; c >= j; c--) // counting repeated numbers
+                            if (int.Parse(data[c]) == temp) // if number is the same
+                            {
+                                nums++;
+                            }
+                            else // if number is NOT the same
+                            {
+                                if (count <= nums)
+                                {
+                                    data[i] = data[j];
+                                    data[j] = space.ToString();
+                                    break;
+                                }
+
+                                count = 0;
+                                nums = 0;
+                                break;
+                            }
                 }
             }
-        }
 
         return data;
     }
--- Program.cs
+++ Program.cs
@@ -14,23 +14,27 @@ internal static class Program
         /* First Solution */
         sw1.Start();
         
-        Console.Write("Solution 1: " + solution.Solve1(@"Z:\source\AdventOfCode2024\Day9\input.txt")
-            , Console.ForegroundColor = ConsoleColor.DarkGreen); Console.ResetColor();
+        Console.Write("Solution 1: " + solution.Solve1(@"Z:\source\AdventOfCode2024\Day9\input.txt"),
+            Console.ForegroundColor = ConsoleColor.DarkGreen);
+        Console.ResetColor();
         
         sw1.Stop();
         
-        Console.WriteLine(" | Elapsed Time: " + sw1.Elapsed, Console.ForegroundColor == ConsoleColor.Blue);
+        Console.WriteLine(" | Elapsed Time: " + sw1.Elapsed,
+            Console.ForegroundColor == ConsoleColor.Blue);
         Console.ResetColor();
         
         /* Second Solution */
         sw2.Start();
         
-        Console.Write("Solution 2: " + solution.Solve2(@"Z:\source\AdventOfCode2024\Day9\input.txt")
-            , Console.ForegroundColor = ConsoleColor.DarkGreen); Console.ResetColor();
+        Console.Write("Solution 2: " + solution.Solve2(@"Z:\source\AdventOfCode2024\Day9\input.txt"),
+            Console.ForegroundColor = ConsoleColor.DarkGreen);
+        Console.ResetColor();
         
         sw2.Stop();
         
-        Console.WriteLine(" | Elapsed Time: " + sw2.Elapsed, Console.ForegroundColor == ConsoleColor.Blue);
+        Console.WriteLine(" | Elapsed Time: " + sw2.Elapsed,
+            Console.ForegroundColor == ConsoleColor.Blue);
         Console.ResetColor();
     }
 }
